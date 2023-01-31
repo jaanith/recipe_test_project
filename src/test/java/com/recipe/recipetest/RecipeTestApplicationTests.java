1bccadf9bddb3b79e3ba330d18e29252b494c79c
margus.ratsep <margus.ratsep@ttu.ee>
2023-01-31 15:50:34 +0200

    Initial commit

---
+++ src/test/java/com/recipe/recipetest/RecipeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.recipetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
