f124decf929ed4d5c7e9ffaee32589c2dbb3be23
margus.ratsep <margus.ratsep@ttu.ee>
2023-02-07 10:31:00 +0200

    test fix 1

--- src/test/java/com/recipe/recipetest/RecipeTestApplicationTests.java
+++ src/test/java/com/recipe/recipetest/RecipeTestApplicationTests.java
@@ -1,8 +1,11 @@
 package com.recipe.recipetest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
+@DataMongoTest
 @SpringBootTest
 class RecipeTestApplicationTests {
 
--- src/test/java/com/recipe/recipetest/repositories/UnitOfMeasureRepositoryTest.java
+++ src/test/java/com/recipe/recipetest/repositories/UnitOfMeasureRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataMongoTest
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class UnitOfMeasureRepositoryTest {
